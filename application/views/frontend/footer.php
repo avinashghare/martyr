05fbf1f365f85cc3469c7413c1d6b769849648af
mahesh-wohlig <mahesh@wohlig.com>
2015-01-20 13:38:40 +0530

    dmeo
    demo

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -5,16 +5,7 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <script src="<?php echo base_url('frontassets/js/jquery.hoverdir.js'); ?>"></script>
-<script>
-    $(document).ready(function () {
 
-        $(".section1 .show").click(function () {
-            //                console.log("ABCD");
-            $(".section1 span.tobedone").fadeOut(300);
-            $(this).parents(".section1").children("span.tobedone ").fadeIn(300);
-        });
-    });
-</script>
 <script type="text/javascript">
 			$(function() {
 			
--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -16,7 +16,7 @@ $cats2=$category->children[1]->children;
     <div class="row">
         <div class="col-md-12">
             <div class="defence">
-
+                <div class="row">
                 <div class="col-md-6 ">
                     <div class="ministri text-center">
                         <div class="section1">
@@ -54,9 +54,9 @@ $cats2=$category->children[1]->children;
                         <div class="section1 menu">
                             <i class="show img-po"><img src="<?php echo base_url('assets/images/homeaffair1.png'); ?>"></i>
 
-                            <span class="tobedone done">
+                            <span class="tobedone done pull-right">
                                    
-                      <ul class="cssMenu">
+                      <ul class="cssMenu cssMenu2">
                       
 	<?php foreach($cats1 as $cat) {
                           ?>
@@ -82,8 +82,18 @@ $cats2=$category->children[1]->children;
                         </div>
                     </div>
                 </div>
-
+</div>
             </div>
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function () {
+
+        $(".section1 .show").click(function () {
+            //                console.log("ABCD");
+            $(".section1 span.tobedone").fadeOut(0);
+            $(this).parents(".section1").children("span.tobedone ").fadeIn(0);
+        });
+    });
+</script>
